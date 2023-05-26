e8858887aa6f067ee42889ec9908880810e0fac9
Tien Dat <tiendatthsp@gmail.com>
2023-05-26 23:47:23 +0700

    remove chart

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
-import Login from "./scenes/login/Login";
-import LandingPage from "./scenes/landingpage/LandingPage"
-import  Article  from "./scenes/article_post/Article";
-import CreateArticle from "./scenes/article_post/CreateArticle";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import RegistrationForm from "./scenes/Register/Register";
+import Article from "./scenes/article_post/Article";
+import CreateArticle from "./scenes/article_post/CreateArticle";
+import Calendar from "./scenes/calendar/calendar";
+import Contacts from "./scenes/contacts";
+import Dashboard from "./scenes/dashboard";
+import FAQ from "./scenes/faq";
+import Form from "./scenes/form";
+import Sidebar from "./scenes/global/Sidebar";
+import Topbar from "./scenes/global/Topbar";
+import Invoices from "./scenes/invoices";
+import LandingPage from "./scenes/landingpage/LandingPage";
+import Login from "./scenes/login/Login";
 import ProfilePage from "./scenes/profile/Profile";
-import { useEffect } from "react";
+import Team from "./scenes/team";
+import { ColorModeContext, useMode } from "./theme";
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
@@ -54,13 +49,9 @@ function App() {
                 <Route path="/createpost" element={<CreateArticle />} />
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
-                <Route path="/form" element={<Form />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/line" element={<Line />} />
+                <Route path="/form" element={<Form />} />             
                 <Route path="/faq" element={<FAQ />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/geography" element={<Geography />} />
+                <Route path="/calendar" element={<Calendar />} />               
               </Routes>
             </main>
           </div>
