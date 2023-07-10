3e4aeb0dc60fe22fe9f6522d2673774900b0ae39
Tien Dat <tiendatthsp@gmail.com>
2023-07-10 23:06:44 +0700

    fix error in dashboard v3

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,26 +1,15 @@
-import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
-import { tokens } from "../../theme";
-import { mockTransactions } from "../../data/mockData";
-import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-import EmailIcon from "@mui/icons-material/Email";
-import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import TrafficIcon from "@mui/icons-material/Traffic";
-import Header from "../../components/Header";
-import LineChart from "../../components/LineChart";
-import GeographyChart from "../../components/GeographyChart";
-import BarChart from "../../components/BarChart";
-import StatBox from "../../components/StatBox";
-import ProgressCircle from "../../components/ProgressCircle";
-import AddCardIcon from "@mui/icons-material/AddCard";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import PersonPinIcon from "@mui/icons-material/PersonPin";
-import MenuBookIcon from "@mui/icons-material/MenuBook";
-import Apex from "../../components/ApexChart";
-import { useEffect, useState } from "react";
-import axiosClient from "../../utils/axiosCustomize";
 import { FoodBankOutlined } from "@mui/icons-material";
+import AddCardIcon from "@mui/icons-material/AddCard";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import PersonPinIcon from "@mui/icons-material/PersonPin";
+import { Box, Typography, useTheme } from "@mui/material";
 import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import Apex from "../../components/ApexChart";
+import Header from "../../components/Header";
+import StatBox from "../../components/StatBox";
+import { tokens } from "../../theme";
+import axiosClient from "../../utils/axiosCustomize";
 const Dashboard = () => {
   const [data, setData] = useState({
     CurrentMonthProfits: 0,
@@ -229,7 +218,7 @@ const Dashboard = () => {
               Recent Transactions
             </Typography>
           </Box>
-          {data.FiveNewOrders.map((transaction, i) => (
+          {data.FiveNewOrders && data.FiveNewOrders.map((transaction, i) => (
             <Box
               key={`${transaction.ID}-${i}`}
               display="flex"
